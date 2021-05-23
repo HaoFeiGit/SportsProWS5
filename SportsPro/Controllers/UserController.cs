6fa7fbbc1a4d6958a480c34c246ecec659bd2eae
HaoFeiGit <fei1@ualberta.ca>
2021-05-23 02:02:17 -0600

    productReady

---
+++ SportsPro/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsPro.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            // authenticate
+
+
+            return View();
+        }
+    }
+}
